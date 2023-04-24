6ab1dd5e375129c81cc9743b66da5cbea09acec7
OlesjaD <olesja.d.c@gmail.com>
2023-04-25 00:49:13 +0300

    profile css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,25 +9,25 @@ export const Profile =({username, tag, location, avatar, stats}) => {
         <img
             src={avatar}
             alt="User avatar"
-            className="avatar"
+            className={css.avatar}
         />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
         </div>
   
-        <ul className="stats" key= {username}>
+        <ul className={css.stats}>
         <li>
-            <span className="label">Followers</span>
-            <span className="quantity"> {stats.followers}</span>
+            <span className={css.label}>Followers </span>
+            <span className={css.quantity}>{stats.followers}</span>
         </li>
         <li>
-            <span className="label">Views</span>
-            <span className="quantity"> {stats.views}</span>
+            <span className={css.label}>Views </span>
+            <span className={css.quantity}>{stats.views}</span>
         </li>
         <li>
-            <span className="label">Likes</span>
-            <span className="quantity"> {stats.likes}</span>
+            <span className={css.label}>Likes </span>
+            <span className={css.quantity}>{stats.likes}</span>
         </li>
         </ul>
   </div>)
@@ -38,7 +38,9 @@ Profile.propTypes = {
     tag: PropTypes.string.isRequired,
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
+    stats: {
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+    }
 }
